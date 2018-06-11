73ca7a0ce86b4878097e6eae85e9ddfd1023e03e
Anwesh43 <anweshthecool0@gmail.com>
2018-06-11 06:40:07 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedarrowlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedarrowlineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.kotlinlinkedarrowlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linkedarrowlineview.LinkedArrowLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LinkedArrowLineView.create(this)
     }
 }
