1b7dc729fe9b70f8b221de5cc613c634df3b2ac8
Anwesh43 <anweshthecool0@gmail.com>
2018-06-11 06:01:17 +0530

    adding LinkedArrowLineView class

--- linkedarrowlineview/src/main/java/com/anwesh/uiprojects/linkedarrowlineview/LinkedArrowLineView.kt
+++ linkedarrowlineview/src/main/java/com/anwesh/uiprojects/linkedarrowlineview/LinkedArrowLineView.kt
@@ -4,3 +4,26 @@ package com.anwesh.uiprojects.linkedarrowlineview
  * Created by anweshmishra on 11/06/18.
  */
 
+import android.content.*
+import android.graphics.*
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+
+class LinkedArrowLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
