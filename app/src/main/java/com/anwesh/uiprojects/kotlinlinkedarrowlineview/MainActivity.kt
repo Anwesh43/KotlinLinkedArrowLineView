e5baa95c824e449b31334a23ed2e061350486a76
Anwesh43 <anweshthecool0@gmail.com>
2018-06-11 06:45:15 +0530

    adding fullScreen support

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedarrowlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedarrowlineview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.kotlinlinkedarrowlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.linkedarrowlineview.LinkedArrowLineView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LinkedArrowLineView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
